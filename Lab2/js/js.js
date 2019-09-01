abc5cb85de1179dad5a834be06c90ef469ad331c
danieltijerina <dtg.danieltijerina@gmail.com>
2019-09-01 00:05:17 -0500

    Basic functionality for TODO list.

--- Lab2/js/js.js
+++ Lab2/js/js.js
@@ -1,5 +1,37 @@
-var checkbox = document.querySelector("input[name=checkbox]");
+function toggleCheck(element) {
+    if (element.checked) {
+        element.nextSibling.classList.add("done");
+    } else {
+        element.nextSibling.classList.remove("done");
+    }
+}
 
-checkbox.addEventListener( 'change', function() {
+function test() {
     alert("hi");
+}
+
+function addElement(chore) {
+    let list = document.getElementById("items");
+    let node = document.createElement("li");
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    input.onclick = function () {
+        toggleCheck(this);
+    };
+    let span = document.createElement("span");
+    let text = document.createTextNode(chore);
+    span.append(text);
+    node.append(input);
+    node.append(span);
+    list.append(node);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("newitem").addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            addElement(this.value);
+            this.value = "";
+        }
+    });
 });
