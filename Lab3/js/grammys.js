6f5c09346885e497ae0bdfc22a617eec0c050869
Daniel Tijerina González <dtg.danieltijerina@gmail.com>
2019-09-15 21:57:34 -0500

    Update grammys.js

--- Lab3/js/grammys.js
+++ Lab3/js/grammys.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   $.getJSON('https://danieltijerina.github.io/LabWeb2019/Lab3/data/grammys.json', function(info) {
     var items = [];
     $.each(info, function(index, value) {
-      	items.push("<option value='" + index + "'>" + value.field + "<option/>");
+        console.log(info);
+      	items.push("<option value='" + index + "'>" + value.field + "</option>");
     });
     $('#category_types').append(items);
   });
