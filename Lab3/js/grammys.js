347a7debd62a554c194e9056fcb892acd805b3c0
Daniel Tijerina González <dtg.danieltijerina@gmail.com>
2019-09-15 21:46:59 -0500

    Initial Lab3 files

---
+++ Lab3/js/grammys.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $.getJSON('../data/grammys.json', function(data) {
+      var items = [];
+      $.each(data, function(index, value) {
+      	  items.push("<option value='" + index + "'>" + value.field + "<option/>");
+      });
+      $('#category_types').append(items);
+    });
+
+    $('#category_types').on('change', function() {
+    console.log($(this).value);
+  });
+});
