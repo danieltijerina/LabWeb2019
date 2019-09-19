ead85f61de4472ec3f322e0fd972206f53768769
danieltijerina <dtg.danieltijerina@gmail.com>
2019-09-19 17:40:54 -0500

    Moved code to be more readable.

--- EX1/js/js.js
+++ EX1/js/js.js
@@ -5,50 +5,15 @@
    on click!
    (5 puntos)
 */
-
-
+$('#escribe_reseña').click(function () {
+  $('#seccion_comentario').toggleClass('hidden');
+});
 
 /*
 2. Cargar los comentarios de el archivo comentarios.xml o bien de 
   https://tc2026daw.github.io/instrucciones/misc/comentarios.xml 
   (función ajax, 25 puntos)
 */
-
-
-/*
-3. Funcion que apendiza el nuevo comentario al darle click a PUBLICAR
-  on click!
-  (función, 35 puntos)
-*/
-
-
-/*
-4. Funcion que limpia el nombre, el email y el div "#comentarios" al darle
-   click en "btn-limpiar" con leyenda de "CANCELAR"
-   on click!
-  (5 puntos)
-*/
-
-
-/*
-Funcion que recibe un numero de stars y regresa los 5 spans 
-que simbolizan las estrellas del rating. por ejemplo:
-let stars = 3;
-let html = getStarsSpans(stars);
-
-html = '
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star checked"></span>
-<span class="fa fa-star"></span>
-<span class="fa fa-star"></span>
-'
-*/
-
-$('#escribe_reseña').click(function () {
-  $('#seccion_comentario').toggleClass('hidden');
-});
-
 $.ajax({
   url: 'https://tc2026daw.github.io/instrucciones/misc/comentarios.xml',
   type: 'GET',
@@ -79,23 +44,11 @@ $.ajax({
   },
 });
 
-function getStarsSpans(stars) {
-  let new_html = '';
-  for (let i = 0; i < stars; i++) {
-    new_html += `
-      <span class="fa fa-star checked"></span>
-    `;
-  }
-
-  for (let i = 0; i < 5 - stars; i++) {
-    new_html += `
-      <span class="fa fa-star"></span>
-    `;
-  }
-
-  return new_html;
-}
-
+/*
+3. Funcion que apendiza el nuevo comentario al darle click a PUBLICAR
+  on click!
+  (función, 35 puntos)
+*/
 $('#btn-publicar').click(function () {
   if ($('#nombre').val() == "" || $('#comentario').text() == "") {
     $('#error_comment').toggleClass('hidden');
@@ -122,6 +75,12 @@ $('#btn-publicar').click(function () {
   }
 });
 
+/*
+4. Funcion que limpia el nombre, el email y el div "#comentarios" al darle
+   click en "btn-limpiar" con leyenda de "CANCELAR"
+   on click!
+  (5 puntos)
+*/
 $('#btn-limpiar').click(function() {
   $("input[name='rating']:checked").val(0);
   $('#nombre').val('');
@@ -130,3 +89,34 @@ $('#btn-limpiar').click(function() {
 
   $('#seccion_comentario').toggleClass('hidden');
 });
+
+/*
+Funcion que recibe un numero de stars y regresa los 5 spans 
+que simbolizan las estrellas del rating. por ejemplo:
+let stars = 3;
+let html = getStarsSpans(stars);
+
+html = '
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+'
+*/
+function getStarsSpans(stars) {
+  let new_html = '';
+  for (let i = 0; i < stars; i++) {
+    new_html += `
+      <span class="fa fa-star checked"></span>
+    `;
+  }
+
+  for (let i = 0; i < 5 - stars; i++) {
+    new_html += `
+      <span class="fa fa-star"></span>
+    `;
+  }
+
+  return new_html;
+}
